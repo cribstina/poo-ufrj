acfbbea337406c899d5bd1f3df2b1c5798a17a28
Cristina Souza <cristinaribs@gmail.com>
2023-06-26 09:58:37 -0300

    adicionando getters e setters

--- trabalho_pratico/src/Comentario.java
+++ trabalho_pratico/src/Comentario.java
@@ -7,4 +7,32 @@ public class Comentario {
     private boolean fixado;
     private int tamanho;
     private String texto;
+
+    public LocalDateTime getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean getFixado(boolean fixado) {
+        return this.fixado;
+    }
+
+    public void setFixado(boolean fixado) {
+        this.fixado = fixado;
+    };
 }
